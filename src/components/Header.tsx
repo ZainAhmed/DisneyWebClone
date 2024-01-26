d52e06b3d0403b5d7df8b07de5d09f95a9095b0d
Zain Ahmed Sohail <zain.ahmed.sohail@gmail.com>
2024-01-26 16:40:23 +0100

    created header with theme tog

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeToggler } from "./ThemeToggler";
+
+function Header() {
+  return (
+    <header
+      className="fixed w-full z-20 top-0 flex justify-between 
+    items-center p-5 bg-gradient-to-t from-gray-200/0
+     via-gray-800/25 to-gray-900"
+    >
+      <Link href="/" className="mr-10">
+        <Image
+          src="https://links.papareact.com/a943ae"
+          alt="Disney Logo"
+          width={120}
+          height={100}
+          className="cursor-pointer invert-0 dark:invert"
+        />
+      </Link>
+      <div className="flex space-x-2">
+        {/* GenreDropDown */}
+        {/* SearchInput */}
+        <ThemeToggler />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
