ae85d5492d2ba15045a165b36055e7326e2a8405
Zain Ahmed Sohail <zain.ahmed.sohail@gmail.com>
2024-01-29 16:41:41 +0100

    created movieCarousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import MoviesCarousel from "@/components/MoviesCarousel";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpComingMovies,
+} from "@/lib/getMovies";
+
+export default async function Home() {
+  const upcomingMovies = await getUpComingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
   return (
     <main className="">
-      <h1>Lets build Disney</h1>
+      {/* CarouselBannerWrapper*/}
+      <div
+        className="flex flex-col space-y-2"
+        // xl:-mt-48"
+      >
+        <MoviesCarousel movies={upcomingMovies} title="Upcoming" isVertical />
+        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousel movies={popularMovies} title="Popular" />
+      </div>
     </main>
   );
 }
