091491c513da6ebee2ecbacd896c7bc1f98b9863
Zain <zain.sohail@gmail.com>
2024-02-22 21:43:01 +0100

    created movie and tv images end points

---
+++ src/app/apis/tv/Images/[id]/route.ts
@@ -0,0 +1,10 @@
+import { baseURL, fetchFromTMDB } from "@/lib/utils";
+import { NextResponse } from "next/server";
+type PropsType = {
+  params: { id: string };
+};
+export async function GET(request: Request, { params }: PropsType) {
+  let url = new URL(`${baseURL}/tv/${params.id}/images`);
+  const response = await fetchFromTMDB(url, true);
+  return NextResponse.json(response);
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,32 +2,11 @@ import {
   MovieDetails,
   SearchResults,
   TvShowDetails,
+  VideoImages,
 } from "@/Types/ComponentTypes";
-
-const baseURL = "https://api.themoviedb.org/3";
-const moviesURL = `${baseURL}/movie`;
 
 const apiBaseUrl = "http://localhost:3000/apis";
 
-async function fetchFromTMDB(url: URL) {
-  url.searchParams.set("include_adult", "false");
-  url.searchParams.set("include_video", "false");
-  url.searchParams.set("language", "en-US");
-  url.searchParams.set("page", "1");
-  url.searchParams.set("sort_by", "popularity.desc");
-
-  const options: RequestInit = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
-    },
-    next: {
-      revalidate: 60 * 6 * 24, //24 hours
-    },
-  };
-  return await fetch(url.toString(), options);
-}
 async function getVideoListResults(response: Response) {
   const data = (await response.json()) as SearchResults;
   return data.results;
@@ -35,6 +14,10 @@ async function getVideoListResults(response: Response) {
 
 async function getVideoDetail(response: Response) {
   return (await response.json()) as TvShowDetails | MovieDetails;
+}
+
+async function getVideoImageDetail(response: Response) {
+  return (await response.json()) as VideoImages;
 }
 
 export async function getUpComingMovies() {
@@ -77,7 +60,12 @@ export async function getTvShowDetails(id: string) {
   return getVideoDetail(res);
 }
 
-// export async function getMovieImages(id: string) {
-//   const url = new URL(`${baseURL}/movie/${id}/images`);
-//   const reponse = await fetchFromTMDB(url);
-// }
+export async function getMovieImages(id: string) {
+  const res = await fetch(`${apiBaseUrl}/movies/details/${id}`);
+  return getVideoImageDetail(res);
+}
+
+export async function getTvImages(id: string) {
+  const res = await fetch(`${apiBaseUrl}/tv/details/${id}`);
+  return getVideoImageDetail(res);
+}
