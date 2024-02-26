65f24aabb2bd417a76e07244e33e34169e28d5f9
Zain <zain.sohail@gmail.com>
2024-02-26 17:29:58 +0100

    styled loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -4,7 +4,12 @@ import { ClipLoader } from "react-spinners";
 function LoadingSpinner() {
   return (
     <div className="bg-slate-300/10 h-[100vh] w-full flex items-center justify-center">
-      <ClipLoader color="white" loading={true} size={150} />
+      <ClipLoader
+        className="border-b-[#040714] border-t-[#040714] border-r-[#040714]  border-l-[#040714]  
+        dark:!border-b-white dark:!border-t-white dark:!border-r-white dark:!border-l-white "
+        loading={true}
+        size={150}
+      />
     </div>
   );
 }
